fb4133c4bb462fcc47c569491bf0a91a4601303a
peaceiris <30958501+peaceiris@users.noreply.github.com>
2018-09-27 17:08:37 +0900

    add abbreviation.tsv

--- add_abbreviation.sh
+++ add_abbreviation.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-
 read -p "abbreviation: " abb
 read -p "expanded: " expanded
-
 echo "${abb}\t${expanded}\t名詞" >> abbreviation.txt
-
 sort -u abbreviation.txt -o abbreviation.txt
-
 git diff
-
-git add abbreviation.txt
+cp abbreviation.txt abbreviation.tsv
+git add abbreviation.txt abbreviation.tsv
 git commit -m "Script: add ${abb} (${expanded})"
