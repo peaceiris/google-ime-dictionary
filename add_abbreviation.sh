fd2accaacad26edbfce1d03c223880c7ccd59a4c
peaceiris <30958501+peaceiris@users.noreply.github.com>
2018-10-04 20:13:08 +0900

    add: set -ue -o pipefail, export LC_ALL=C

--- add_abbreviation.sh
+++ add_abbreviation.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-set -eu
+
+# Fail on unset variables and command errors
+set -ue -o pipefail
+# Prevent commands misbehaving due to locale differences
+export LC_ALL=C
+
 read -p "abbreviation: " abb
 read -p "expanded: " expanded
 echo "${abb}\t${expanded}\t名詞" >> abbreviation.txt
