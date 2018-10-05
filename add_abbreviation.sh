d3c07f3dec50d42f6f3111997df1610e4268cf07
peaceiris <30958501+peaceiris@users.noreply.github.com>
2018-10-05 15:33:58 +0900

    fix

--- add_abbreviation.sh
+++ add_abbreviation.sh
@@ -2,12 +2,10 @@
 
 # Fail on unset variables and command errors
 set -ue -o pipefail
-# Prevent commands misbehaving due to locale differences
-export LC_ALL=C
 
 read -p "abbreviation: " abb
 read -p "expanded: " expanded
-echo "${abb}\t${expanded}\t名詞" >> abbreviation.txt
+echo -e "${abb}\t${expanded}\t名詞" >> abbreviation.txt
 sort -u abbreviation.txt -o abbreviation.txt
 cp abbreviation.txt ./tsv/abbreviation.tsv
 git diff
