e5c8d7239abb19191d97743d22a9a5c02f5f374e
peaceiris <30958501+peaceiris@users.noreply.github.com>
2019-01-07 20:18:08 +0900

    Add tsv header

--- add_abbreviation.sh
+++ add_abbreviation.sh
@@ -8,6 +8,8 @@ read -p "expanded: " expanded
 echo -e "${abb}\t${expanded}\t名詞" >> abbreviation.txt
 sort -u abbreviation.txt -o abbreviation.txt
 cp abbreviation.txt ./tsv/abbreviation.tsv
+sed -i '1iShort\tExpanded' ./tsv/abbreviation.tsv
+
 git diff
 git add abbreviation.txt ./tsv/abbreviation.tsv
 git commit -m "Script: add ${abb} (${expanded})"
