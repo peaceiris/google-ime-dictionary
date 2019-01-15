40df70e0f944602c855da6b427339699d0d93c96
peaceiris <30958501+peaceiris@users.noreply.github.com>
2019-01-15 22:48:41 +0900

    Add process for ATOK

--- add_abbreviation.sh
+++ add_abbreviation.sh
@@ -7,8 +7,13 @@ read -p "abbreviation: " abb
 read -p "expanded: " expanded
 echo -e "${abb}\t${expanded}\t名詞" >> abbreviation.txt
 sort -u abbreviation.txt -o abbreviation.txt
+
 cp abbreviation.txt ./tsv/abbreviation.tsv
 sed -i '1iReading\tWord\tCategory' ./tsv/abbreviation.tsv
+
+cp abbreviation.txt ./atok/abbreviation.utf16.txt
+sed -i '1i!!ATOK_TANGO_TEXT_HEADER_1' ./atok/abbreviation.utf16.txt
+nkf -w16 --overwrite ./atok/abbreviation.utf16.txt
 
 git diff
 git add abbreviation.txt ./tsv/abbreviation.tsv
