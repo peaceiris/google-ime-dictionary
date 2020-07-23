71ddd921bb324189f68ac13b90384b96e7c41be2
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-07-23 16:00:20 +0900

    ci: Add release workflow

--- release.sh
+++ release.sh
@@ -3,5 +3,22 @@
 # Fail on unset variables and command errors
 set -ue -o pipefail
 
-zip abbreviation.zip abbreviation.txt
-mv abbreviation.zip ~/Desktop/
+
+TAG_NAME="$1"
+RELEASE_ASSETS_SOURCE="abbreviation day_month edict-googleime edict-googleime_us"
+RELEASE_ASSETS_DIR="build"
+RELEASE_NOTES="release_notes.md"
+
+
+\rm -rf "${RELEASE_ASSETS_DIR}" || true
+mkdir "${RELEASE_ASSETS_DIR}"
+for i in ${RELEASE_ASSETS_SOURCE}; do
+    zip "./${RELEASE_ASSETS_DIR}/${i}.zip" "./dict/${i}.txt"
+done
+
+sed -i "1iIME 拡張辞書 ${TAG_NAME}\n" "./${RELEASE_NOTES}"
+
+hub release edit \
+    --file "./${RELEASE_NOTES}" \
+    --attach "$(for i in $(\ls ./${RELEASE_ASSETS_DIR}/*); do echo -n "${i}#${i} "; done)" \
+    "${TAG_NAME}"
---
+++ release_notes.md
@@ -0,0 +1,19 @@
+
+
+<!--
+
+ファイル名に `utf16` が含まれるファイルは ATOK 用です。
+
+- Google 日本語入力 👉 `*.zip`
+- ATOK 👉 `*.utf16.zip`
+
+-->
+
+## ファイル名と辞書の対応関係
+
+| ファイル名 | 中身 |
+|---|---|
+| abbreviation.zip | 英語略語展開のための IME 拡張辞書 |
+| day_month.zip | 英語の月と曜日を素早く入力するための IME 拡張辞書 |
+| edict-googleime.zip | 日本語を英語に変換するための JIS キーボード向け IME 拡張辞書 |
+| edict-googleime_us.zip | 日本語を英語に変換するための US キーボード向け IME 拡張辞書 |
