31ea7841d3b791dcdd90d36190da670151c18c7d
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-07-23 14:12:12 +0900

    mkdir dict

--- add_abbreviation.sh
+++ add_abbreviation.sh
@@ -3,13 +3,16 @@
 # Fail on unset variables and command errors
 set -ue -o pipefail
 
+ABB_TXT_PATH="./dict/abbreviation.txt"
+ABB_TSV_PATH="./tsv/abbreviation.tsv"
+
 read -p "abbreviation: " abb
 read -p "expanded: " expanded
-echo -e "${abb}\t${expanded}\t名詞" >> abbreviation.txt
-sort -u abbreviation.txt -o abbreviation.txt
-cp abbreviation.txt ./tsv/abbreviation.tsv
-sed -i '1iReading\tWord\tCategory' ./tsv/abbreviation.tsv
+echo -e "${abb}\t${expanded}\t名詞" >> "${ABB_TXT_PATH}"
+sort -u "${ABB_TXT_PATH}" -o "${ABB_TXT_PATH}"
+cp "${ABB_TXT_PATH}" "${ABB_TSV_PATH}"
+sed -i '1iReading\tWord\tCategory' "${ABB_TSV_PATH}"
 
 git diff
-git add abbreviation.txt ./tsv/abbreviation.tsv
-git commit -m "Script: add ${abb} (${expanded})"
+git add "${ABB_TXT_PATH}" "${ABB_TSV_PATH}"
+git commit -m "abb: add ${abb} (${expanded})"
