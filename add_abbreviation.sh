bc6eaab323266c94ab904410ea3a43b934cfe416
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-11-27 17:09:11 +0900

    add lower_expanded title_expanded
    related to #31

--- add_abbreviation.sh
+++ add_abbreviation.sh
@@ -8,7 +8,10 @@ ABB_TSV_PATH="./tsv/abbreviation.tsv"
 
 read -p "abbreviation: " abb
 read -p "expanded: " expanded
-echo -e "${abb}\t${expanded}\t名詞" >> "${ABB_TXT_PATH}"
+lower_expanded=$(echo ${expanded} | sed -e 's/[A-Z]/\L&/g;')
+title_expanded=$(echo ${expanded} | sed -e 's/[A-Z]/\L&/g; s/\s[a-z]/\U&/g; s/^[a-z]/\U&/;')
+echo -e "${abb}\t${lower_expanded}\t名詞" >> "${ABB_TXT_PATH}"
+echo -e "${abb}\t${title_expanded}\t名詞" >> "${ABB_TXT_PATH}"
 sort -u "${ABB_TXT_PATH}" -o "${ABB_TXT_PATH}"
 cp "${ABB_TXT_PATH}" "${ABB_TSV_PATH}"
 sed -i '1iReading\tWord\tCategory' "${ABB_TSV_PATH}"
